f20245e5d7954bae1da03d8bcab0b69992d7cf44
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2017-11-28 01:55:44 +0200

    fix decoding of multipart data parts containing control chars

--- src/multipart.lua
+++ src/multipart.lua
@@ -35,49 +35,98 @@ local function decode(body, boundary)
   local part_headers = {}
   local part_index = 1
   local part_name, part_value, part_value_ct
+  local processing_part_value = false
   part_value = {}
   part_value_ct = 0
 
-  for line in body:gmatch("[^\r\n]+") do
-    if line:sub(1, 2) == "--" and line:sub(3, #boundary+2) == boundary then
-      if part_name ~= nil then
-        result.data[part_index] = {
-          name = part_name,
-          headers = part_headers,
-          value = table.concat(part_value, "\r\n")
-        }
+  local position = 1
+  local done = false
 
-        result.indexes[part_name] = part_index
+  repeat
 
-        -- Reset fields for the next part
-        part_headers = {}
-        part_value = {}
-        part_value_ct = 0
-        part_name = nil
-        part_index = part_index + 1
+    local s = string.find(body, "[\r\n]", position)
+
+    local line
+
+    if s then
+      line = string.sub(body, position, s - 1)
+      position = s + 1
+    else
+      if position == 1 then
+        line = body
+      else
+        line = string.sub(body, position)
       end
-    elseif line:sub(1, 19):lower() == "content-disposition" then --Beginning of part
-      -- Extract part_name
-      for v in line:gmatch("[^;]+") do
-        if not is_header(v) then -- If it's not content disposition part
-          local pos = v:match("^%s*[Nn][Aa][Mm][Ee]=()")
-          if pos then
-            local current_value = v:match("^%s*([^=]*)", pos):gsub("%s*$", "")
-            part_name = string.sub(current_value, 2, string.len(current_value) - 1)
+
+      done = true
+    end
+
+    if line == "" then
+      if processing_part_value then
+        part_value_ct = part_value_ct + 1
+        part_value[part_value_ct] = string.sub(body, s, s)
+      end
+    else
+      if line:sub(1, 2) == "--" and line:sub(3, #boundary+2) == boundary then
+        processing_part_value = false
+
+        if part_name ~= nil then
+          if part_value[part_value_ct] == "\n" then
+            part_value[part_value_ct] = nil
+          end
+
+          if part_value[part_value_ct-1] == "\r" then
+            part_value[part_value_ct-1] = nil
+          end
+
+          result.data[part_index] = {
+            name = part_name,
+            headers = part_headers,
+            value = table.concat(part_value)
+          }
+
+          result.indexes[part_name] = part_index
+
+          -- Reset fields for the next part
+          part_headers = {}
+          part_value = {}
+          part_value_ct = 0
+          part_name = nil
+          part_index = part_index + 1
+        end
+      elseif line:sub(1, 19):lower() == "content-disposition" then --Beginning of part
+        processing_part_value = false
+
+        -- Extract part_name
+        for v in line:gmatch("[^;]+") do
+          if not is_header(v) then -- If it's not content disposition part
+            local pos = v:match("^%s*[Nn][Aa][Mm][Ee]=()")
+            if pos then
+              local current_value = v:match("^%s*([^=]*)", pos):gsub("%s*$", "")
+              part_name = string.sub(current_value, 2, string.len(current_value) - 1)
+            end
+          end
+        end
+        table.insert(part_headers, line)
+      else
+        if is_header(line) then
+          processing_part_value = false
+          table.insert(part_headers, line)
+        else
+          processing_part_value = true
+          -- The value part begins
+          part_value_ct = part_value_ct + 1
+          part_value[part_value_ct] = line
+          if s then
+            part_value_ct = part_value_ct + 1
+            part_value[part_value_ct] = string.sub(body, s, s)
           end
         end
       end
-      table.insert(part_headers, line)
-    else
-      if is_header(line) then
-        table.insert(part_headers, line)
-      else
-        -- The value part begins
-        part_value_ct = part_value_ct + 1
-        part_value[part_value_ct] = line
-      end
     end
-  end
+
+  until done
+
   return result
 end
 
@@ -117,7 +166,7 @@ function MultipartData:get(name)
   return self._data.data[self._data.indexes[name]]
 end
 
-function MultipartData:get_all(name)
+function MultipartData:get_all()
   local result = {}
   for k, v in pairs(self._data.indexes) do
     result[k] = self._data.data[v].value
@@ -126,16 +175,27 @@ function MultipartData:get_all(name)
 end
 
 function MultipartData:set_simple(name, value)
-  self._data.data[table_size(self._data.indexes) + 1] = {
-    name = name,
-    value = value,
-    headers = { "Content-Disposition: form-data; name=\""..name.."\"" }
-  }
+  if self._data.indexes[name] then
+    self._data.data[self._data.indexes[name]] = {
+      name = name,
+      value = value,
+      headers = { "Content-Disposition: form-data; name=\""..name.."\"" }
+    }
+  else
+    local part_index = table_size(self._data.indexes) + 1
+    self._data.indexes[name] = part_index
+    self._data.data[part_index] = {
+      name = name,
+      value = value,
+      headers = { "Content-Disposition: form-data; name=\""..name.."\"" }
+    }
+  end
 end
 
 function MultipartData:delete(name)
   if self._data.indexes[name] then
     self._data.data[self._data.indexes[name]].value = nil
+    self._data.indexes[name] = nil
   end
 end
 
